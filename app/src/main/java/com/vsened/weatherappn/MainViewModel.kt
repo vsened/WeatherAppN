6f6d3306f6722c7f6b7e1164262bd6d26e09efd1
vsened <vasily.senederov@gmail.com>
2023-05-11 10:00:36 +0300

    adding_data_model_class

--- app/src/main/java/com/vsened/weatherappn/MainViewModel.kt
+++ app/src/main/java/com/vsened/weatherappn/MainViewModel.kt
@@ -2,9 +2,12 @@ package com.vsened.weatherappn
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.vsened.weatherappn.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
 
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
+
+
 }
